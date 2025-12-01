2d12c4621a7ae120200cb96e0ed28d6baf52d143
RishabKr15 <126272470+RishabKr15@users.noreply.github.com>
2025-12-01 23:44:42 +0530

    Bump to v0.1.8

--- src/kavachnet/__init__.py
+++ src/kavachnet/__init__.py
@@ -6,4 +6,4 @@ KavachNet - AI-based VPN & Proxy Detector
 from .vpn_checker import is_vpn_ip, refresh_cache, load_networks_from_cache, load_cached_ips
 
 # Define the package version (should match pyproject.toml)
-__version__ = "0.1.7"
+__version__ = "0.1.8"
